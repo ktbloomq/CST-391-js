870952b262336084d6845be4834ede27aa867ce0
Kevin Bloomquist <ktb@bloomquist.com>
2023-09-18 11:50:06 -0700

    week 5 stopping point 3

--- week5/music/README.md
+++ week5/music/README.md
@@ -1,4 +1,10 @@
 # Music App - React
 ## Part 1
 In this part, I started rebuilding the music app in React. It uses Bootstrap cards to display albums. The app uses 3 main files: index.js, App.js, and Card.js. index.js simply renders App.js. App.js creates a bootsrtap grid with cards. Card.js takes in paramaters for the album title, description, image, and the button text.
+
 ![music app](./screenshot1.png)
+
+## Part 2
+In this part, I moved album data to a albumList. More specifically, I used a state to manage the list. This is useful because it allows the contents to be dynamically changed when I implement the music API
+
+![music app](./screenshot2.png)
--- week5/music/src/App.js
+++ week5/music/src/App.js
@@ -1,38 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 
 const App = () => {
+
+    const [albumList, setAlbumList] = useState([
+        {
+        artistId: 0,
+        artist: 'The Beatles',
+        title: 'Yellow Submarine',
+        description:
+        'Yellow Submarine is the tenth studio album by English rock band the Beatles, released on 13 January 1969 in the United States and on 17 January 1969 in the United Kingdom.',
+        year: 1969,
+        image:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/a/ac/TheBeatles-YellowSubmarinealbumcover.jpg/220px-TheBeatles-YellowSubmarinealbumcover.jpg',
+        },
+        {
+        artistId: 1,
+        artist: 'The Beatles',
+        title: 'Abbey Road',
+        description:
+        'Yellow Submarine is the tenth studio album by English rock band the Beatles, released on 13 January 1969 in the United States and on 17 January 1969 in the United Kingdom.',
+        year: 1969,
+        image:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/4/42/Beatles_-_Abbey_Road.jpg/220px-Beatles_-_Abbey_Road.jpg',
+        },
+        {
+        artistId: 2,
+        artist: 'The Beatles',
+        title: 'Let It Be',
+        description:
+        "Let It Be is the twelfth and final studio album by the English rock band the Beatle s. It was released on 8 May 1970, almost a month after the group's break-up.",
+        year: 1970,
+        image:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/2/25/LetItBe.jpg/220px-LetItBe.jpg',
+        },
+    ]);
+
+    const renderedList = function() {
+        return albumList.map((album) => {
+            return (
+                <div className="col">
+                    <Card 
+                    albumTitle={album.title}
+                    albumDescription={album.description}
+                    imgURL={album.image}
+                    buttonText="OK"
+                    />
+                </div>
+            );
+        });
+    };
+
     return (
         <div className="container">
             <h1>Music I like</h1>
 
             <div className="row g-3">
-                <div className="col">
-                <Card 
-                albumTitle="Revolver"
-                albumDescription="Revolver is the seventh studio albums by the English rock band the Beatles. Released on 5 August 1966, it was the Beatles' final recording project before their retirement as live performers and marked the group's most overt use of studio technology up to that time, building on the advances of their late 1965 release Rubber Soul."
-                imgURL="https://m.media-amazon.com/images/I/91ffeWzPNpL._SL1500_.jpg"
-                buttonText="OK"
-                test=''
-                />
+                {renderedList()}
             </div>
-            <div className="col">
-                <Card 
-                albumTitle="Rubber Soul"
-                albumDescription={"Rubber Soul is the sixth studio albums by the English rock band the Beatles. It was released on 3 December 1965 in the United Kingdom, on EMI's Parlophone label, accompanied by the non-albums double A-side single \"Day Tripper\" / \"We Can Work It Out\"."}
-                imgURL="https://m.media-amazon.com/images/I/81EF5zXRFdL._SL1500_.jpg"
-                buttonText="OK"
-                />
-            </div>
-            <div className="col">
-                <Card 
-                albumTitle="Please Please Me"
-                albumDescription="Please Please Me is the debut studio albums by the English rock band the Beatles."
-                imgURL="https://m.media-amazon.com/images/I/61LdKbic+wL.jpg"
-                buttonText="OK"
-                />
-                </div>
-            </div>            
         </div>
     );
 }
